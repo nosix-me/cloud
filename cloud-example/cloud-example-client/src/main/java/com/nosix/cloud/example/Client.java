af740665d65361e532884730a72a24a2b87d5f0a
何亚祥 <yaxiang.he@moji.com>
2016-08-16 22:55:30 +0800

    update code

--- cloud-example/cloud-example-client/src/main/java/com/nosix/cloud/example/Client.java
+++ cloud-example/cloud-example-client/src/main/java/com/nosix/cloud/example/Client.java
@@ -25,11 +25,6 @@ public class Client {
 			} catch (TException e) {
 				e.printStackTrace();
 			}
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
 		}
 		System.out.println(System.currentTimeMillis() - stime);
 	}
